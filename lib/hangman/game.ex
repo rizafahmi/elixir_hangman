5091b04d7f07d05003ba5361c4e3e27e8fbc6f00
Riza Fahmi <rizafahmi@gmail.com>
2017-11-27 17:15:54 +0700

    Testing game :won and :lost

--- lib/hangman/game.ex
+++ lib/hangman/game.ex
@@ -10,4 +10,12 @@ defmodule Hangman.Game do
       letters: Dictionary.random_word() |> String.codepoints()
     }
   end
+
+  def make_move(game = %{ game_state: state }, _guess) when state in [:won, :lost] do
+    { game, tally(game) }
+  end
+  
+  defp tally(_game) do
+    123
+  end
 end
---
+++ test/game_test.exs
@@ -0,0 +1,24 @@
+defmodule GameTest do
+  use ExUnit.Case
+  alias Hangman.Game
+  
+  test "new game returns struct" do
+    game = Game.new_game()
+    assert game.turns_left == 7
+    assert game.game_state == :initializing
+    assert length(game.letters) > 0
+  end
+  
+  test "all letters should be lower case" do
+    game = Game.new_game()
+    caps = Enum.filter(game.letters, fn letter -> Regex.match?(~r/[A-Z]/, letter) end)
+    assert length(caps) < 1
+  end
+  
+  test "State doesnt change for :won or :lost game" do
+    for state <- [:won, :lost] do
+      game = Game.new_game() |> Map.put(:game_state, state)
+      { ^game, _ } = Game.make_move(game, "x")
+    end
+  end
+end
